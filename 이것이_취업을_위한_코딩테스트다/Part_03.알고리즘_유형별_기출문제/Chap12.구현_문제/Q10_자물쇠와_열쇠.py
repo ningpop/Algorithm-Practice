0447bfcc28137f6390a495e78efc56b5df189259
ningpop <craft1933@naver.com>
2021-05-15 18:38:02 +0900

    이코테 Chap12 구현 문제 - Q10 (2nd try)

--- 이것이_취업을_위한_코딩테스트다/Part_03.알고리즘_유형별_기출문제/Chap12.구현_문제/Q10_자물쇠와_열쇠.py
+++ 이것이_취업을_위한_코딩테스트다/Part_03.알고리즘_유형별_기출문제/Chap12.구현_문제/Q10_자물쇠와_열쇠.py
@@ -1,48 +1,33 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60059
 
-# 2차원 리스트 90도 회전
-def rotate_a_matrix_by_90_degree(a: list) -> list:
-    n = len(a) # 행 길이 계산
-    m = len(a[0]) # 열 길이 계산
-    result = [[0] * n for _ in range(m)] # 결과 리스트
-    for i in range(n):
-        for j in range(m):
-            result[j][n - i - 1] = a[i][j]
-    return result
+def rotate_key(key: list) -> list:
+    pass
 
-# 자물쇠의 중간 부분이 모두 1인지 확인
-def check(new_lock: list) -> bool:
-    lock_length = len(new_lock) // 3
-    for i in range(lock_length, lock_length * 2):
-        for j in range(lock_length, lock_length * 2):
-            if new_lock[i][j] != 1:
-                return False
-    return True
+def check_lock(key: list, field: list) -> bool:
+    for i in range(len(field)-len(key)+1):
+        for j in range(len(field)-len(key)+1):
+            for k in range(len(key)):
+                for l in range(len(key)):
+                    field[i+k][j+l] += key[k][l]
+            for k in range(field//3, len(field)):
+                for l in range(field//3, len(field)):
+                    if field[k][l] != 1:
+                        return False
+
+
 
 def solution(key: list, lock: list) -> bool:
-    n = len(lock)
-    m = len(key)
-    # 자물소의 크기를 기존의 3배로 변환
-    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
-    # 새로운 자물쇠의 중앙 부분에 기존의 자물쇠 넣기
-    for i in range(n):
-        for j in range(n):
-            new_lock[i + n][j + n] = lock[i][j]
+    field = []
+    for i in range(len(lock)*3):
+        field.append([0 for z in range(len(lock)*3)])
 
-    # 4가지 방향에 대해서 확인
-    for rotation in range(4):
-        key = rotate_a_matrix_by_90_degree(key)
-        for x in range(n * 2):
-            for y in range(n * 2):
-                # 자물쇠에 열쇠를 끼워 넣기
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] += key[i][j]
-                # 새로운 자물쇠에 열쇠가 정확히 들어맞는지 검사
-                if check(new_lock) == True:
-                    return True
-                # 자물쇠에서 열쇠를 다시 빼기
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] -= key[i][j]
-    return False
+    for i in range(len(lock)):
+        for j in range(len(lock)):
+            field[len(lock)+i][len(lock)+j] = lock[i][j]
+    
+    
+
+
+
+print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
+
