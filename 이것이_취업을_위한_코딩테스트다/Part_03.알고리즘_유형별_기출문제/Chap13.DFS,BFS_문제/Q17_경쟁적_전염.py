628ce2f6139349d2fac8b58993a6407930b4c6d6
ningpop <craft1933@naver.com>
2021-05-23 23:54:13 +0900

    이코테 Chap13 DFS/BFS 문제 - Q17

---
+++ 이것이_취업을_위한_코딩테스트다/Part_03.알고리즘_유형별_기출문제/Chap13.DFS,BFS_문제/Q17_경쟁적_전염.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+plate = []
+for _ in range(N):
+    plate.append(list(map(int, input().split())))
+
+S, X, Y = map(int, input().split())
+
+virus = deque()
+for v in range(1, K+1):
+    for idx, row in enumerate(plate):
+        if v in row:
+            virus.append((v, idx, row.index(v)))
+virus.append((0, 0, 0))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+S -= 1
+while S >= 0:
+    # print(virus)
+    v, i, j = virus.popleft()
+    if v == 0:
+        S -= 1
+        continue
+    for d in range(4):
+        nx = i + dx[d]
+        ny = j + dy[d]
+        if 0 <= nx < N and 0 <= ny < N:
+            if plate[nx][ny] == 0:
+                plate[nx][ny] = v
+                virus.append((v, nx, ny))
+    virus.append((0, 0, 0))
+
+print(plate[X-1][Y-1])
