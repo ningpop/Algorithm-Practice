084c52f2c13d52ade7286512e95e7d411f8d320d
ningpop <craft1933@naver.com>
2022-03-27 18:11:37 +0900

    이코테 Chap12 구현 문제 Q14 오답 및 제한시간 내 미해결 (2회차)

---
+++ 이것이_취업을_위한_코딩테스트다/Part_03.알고리즘_유형별_기출문제/Chap12.구현_문제/220317/Q14.외벽_점검.py
@@ -0,0 +1,12 @@
+# 2022.03.27
+# 제한 시간: 50분 / 풀이 시간 50분 00초
+# 채점 결과: 오답 및 제한시간 내 미해결
+# 시간복잡도: 
+# 문제 링크: https://programmers.co.kr/learn/courses/30/lessons/60062
+
+def solution(n: int, weak: list, dist: list) -> int:
+    answer = 0
+    return answer
+
+print(solution(12, [1, 5, 6, 10], [1, 2, 3, 4]))
+print(solution(12, [1, 3, 4, 9, 10], [3, 5, 7]))
